12f52044979c6ecb6930899fb126af4e9336f9a3
sam <sam1111165@gmail.com>
2022-12-27 21:51:06 +0800

    Update rosrun.sh

--- rosrun.sh
+++ rosrun.sh
@@ -2,6 +2,6 @@
 #OAKD
 # rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/Examples/ROS/OAKD.yaml
 
-rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/Examples/ROS/mycam.yaml
+rosrun ORB_SLAM3 Mono ./ORB_SLAM3/Vocabulary/ORBvoc.txt ./ORB_SLAM3/Examples/ROS/mycam.yaml
 
 # rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/Examples/Monocular/TUM-VI.yaml
